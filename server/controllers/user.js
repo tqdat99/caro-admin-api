63c87c875f02984209e551a6b2ca0eeac2ef620d
khanhhongtranle <hong.tran@dotb.vn>
2020-12-21 22:04:02 +0700

    search users by username or email api

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -6,7 +6,7 @@ jwt_secret_or_key = 'WEBNC17' || process.env.JWT_SECRET_OR_KEY;
 // Get users
 module.exports.getUsers = function (req, res) {
   return User.find()
-    .select('username')
+    .select('username email')
     .then((Users) => {
       return res.status(200).json({
         success: true,
@@ -41,4 +41,29 @@ module.exports.getUserByUsername = function (req, res) {
         error: err.message,
       });
     });
+}
+
+/**
+ * khanhhongtranle
+ */
+// Search user(s) by username or email address
+module.exports.searchUsers = function (req, res){
+    let keyword = req.query.kw;
+    console.log(keyword);
+    return User.find( {$or:[ { username: {$regex : keyword} }, {email: { $regex: keyword}} ]})
+        .select('username email')
+        .then((User) => {
+            return res.status(200).json({
+                success: true,
+                message: 'User',
+                User: User,
+            });
+        })
+        .catch((err) => {
+            res.status(500).json({
+                success: false,
+                message: 'Server error. Please try again.',
+                error: err.message,
+            });
+        });
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getUsers, getUserByUsername } = require('../controllers/user');
+const { getUsers, getUserByUsername, searchUsers } = require('../controllers/user');
 const userRoutes = express.Router();
 var passport = require('passport');
 require('../passport/passport')(passport);
 
 userRoutes.get('/', passport.authenticate('jwt', { session: false }), getUsers);
 userRoutes.get('/user', passport.authenticate('jwt', { session: false }), getUserByUsername);
+userRoutes.get('/search', passport.authenticate('jwt', { session: false }), searchUsers);
 
 module.exports = userRoutes;
